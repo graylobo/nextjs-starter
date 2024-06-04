6637c7608fc74358571b090903ff7947f44d3055
graylobo <cazador.min@gmail.com>
2024-06-04 22:14:30 +0900

    streaming

---
+++ some.ts
@@ -0,0 +1,20 @@
+setTimeout(() => {
+    console.log('Task 1 completed');
+  }, 3000);
+
+
+  setTimeout(() => {
+    console.log('Task 2 completed');
+  }, 3000);
+
+  setTimeout(() => {
+    console.log('Task 3 completed');
+  }, 3000);
+
+  setTimeout(() => {
+    console.log('Task 4 completed');
+  }, 3000);
+
+  setTimeout(() => {
+    console.log('Task 5 completed');
+  }, 3000);
